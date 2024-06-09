ddaf2de02405f2268526cf890366c88c3e68a935
PonchoBT <ponchobt.dev@gmail.com>
2024-06-09 00:35:24 -0600

    corrected

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,43 +1,45 @@
-// import setup for Apollo Server with Express
-const { gql } = require('apollo-server-express');
+const typeDefs = `
+  type Query {
+    me: User
+  }
 
-const typeDefs = gql`
-    type User {
-        _id: ID!
-        username: String!
-        email: String!
-        bookCount: Int
-        savedBooks: [Book]
-    }
-    type Auth {
-        token: ID!
-        user: User
-    }
-    type Book {
-        bookId: ID!
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
-    input InputBook {
-        bookId: String
-        authors: [String]
-        title: String
-        description: String
-        image: String
-        link: String
-    }
-    type Query {
-        me: User
-    }
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(newBook: InputBook!): User
-        removeBook(bookId: ID!): User
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
